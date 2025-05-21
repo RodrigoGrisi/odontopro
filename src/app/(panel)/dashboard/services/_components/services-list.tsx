33bacb4b2fcebb95442cbf2ff7a28d9cebc8eca5
Rodrigo Grisi <Rodricosta1995@gmail.com>
2025-05-20 23:17:25 -0300

    feat: ajusting service-list component

--- src/app/(panel)/dashboard/services/_components/services-list.tsx
+++ src/app/(panel)/dashboard/services/_components/services-list.tsx
@@ -5,15 +5,27 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import { DialogService } from "./dialog-service";
+import { Services } from "@/generated/prisma";
 
 
-export function ServicesList() {
+interface ServicesListProps {
+  services: Services[]
+}
+
+export function ServicesList({ services }: ServicesListProps) {
 
   const [isDialogOpen, setIsDialogOpen] = useState(false)
 
+  const servicesList = services.map((service) => ({
+    id: service.id,
+    name: service.name,
+    price: service.price,
+    duration: service.duration,
+  }));
+
   return (
     <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-      <section className="mx-auto bg-green-100 w-[100%]">
+      <section className="mx-auto  w-[100%]">
 
         <Card>
           <CardHeader className="flex flex-row items-center justify-between">
@@ -23,12 +35,28 @@ export function ServicesList() {
             </DialogTrigger>
             <DialogContent>
 
-              <DialogService  handleClose={() => setIsDialogOpen(false)}   />
-
+              <DialogService handleClose={() => setIsDialogOpen(false)} />
             </DialogContent>
           </CardHeader>
         </Card>
 
+        <div className="flex flex-col justify-between w-full mt-4">
+          {servicesList.map((service) => (
+            <section key={service.id} className=" p-2 my-1 bg-white rounded-lg shadow-md border border-gray-200  flex flex-row justify-between" >
+              <div>
+                <h2 className="text-lg font-semibold">{service.name}</h2>
+                <p className="text-gray-600">
+                  💰 Preço: {service.price} - ⏱ Duração: {service.duration} minutos
+                </p>
+              </div>
+
+              <div className="mt-2 gap-1.5 flex flex-row">
+                <Button className="mr-2 bg-emerald-600 text-white cursor-pointer">Editar</Button>
+                <Button className="mr-2 bg-emerald-600 text-white cursor-pointer">Excluir</Button>
+              </div>
+            </section>
+          ))}
+        </div>
       </section>
     </Dialog>
   )
--- src/app/(panel)/dashboard/services/page.tsx
+++ src/app/(panel)/dashboard/services/page.tsx
@@ -8,31 +8,12 @@ export default async function Service() {
   const session = await getServerSession(authOptions);
   if (!session) return redirect("/");
 
-  const rawServices = await getAllServices({ userId: session.user.id });
-
-  const services = rawServices?.data.map((service) => ({
-    ...service,
-    formattedPrice: (service.price / 100).toLocaleString("pt-BR", {
-      style: "currency",
-      currency: "BRL",
-    }),
-  }));
+  const services = await getAllServices({ userId: session.user.id });
 
   return (
     <section className="flex flex-col h-screen w-full p-4 space-y-4 overflow-y-auto">
       <ServiceContent userId={session.user.id} />
 
-      {services?.map((service) => (
-        <div
-          key={service.id}
-          className="p-4 bg-white rounded-lg shadow-md border border-gray-200"
-        >
-          <h2 className="text-lg font-semibold">{service.name}</h2>
-          <p className="text-gray-600">
-            💰 Preço: {service.formattedPrice} - ⏱ Duração: {service.duration} minutos
-          </p>
-        </div>
-      ))}
     </section>
   );
 }
